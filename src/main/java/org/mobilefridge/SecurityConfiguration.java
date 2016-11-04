8498dc1f10fa0028507ed0254503d98cf98ad67b
Marcin <markowitek@gmail.com>
2016-11-04 17:02:39 +0100

    fixed configuration for spring security, now HTTP POST is working

--- src/main/java/org/mobilefridge/SecurityConfiguration.java
+++ src/main/java/org/mobilefridge/SecurityConfiguration.java
@@ -5,7 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -29,5 +31,12 @@ public class SecurityConfiguration {
         auth.authenticationProvider(accountAuthenticationProvider);
     }
 
+    @Configuration
+    public class RestSecurityConfig extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.csrf().disable();
+        }
+    }
 
 }
